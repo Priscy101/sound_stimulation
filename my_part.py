7e6d7e7bb445f575f7fe23a9652e056c2d7e7f4e
Priscy101 <117815463+Priscy101@users.noreply.github.com>
2023-07-11 00:11:02 +0100

    stim.py

---
+++ my_part.py
@@ -0,0 +1,20 @@
+import math
+
+Human_Hearing_Threshold = 140 
+
+
+def main_in_room():
+    """ Stimulates the program if the user is in the classroom"""
+    # input parameters
+    lawnmower_power = float(input("enter lawnmower power in watts: "))
+    material = input("enter room material(wood, glass, or bricks): ")
+    distance = float(input("enter your distance from from the lawnmower in maters: "))
+
+    # stimulate sound penetration
+    sound_level = math.ceil(stimulate_sound_penetration_in_room(lawnmower_power, material, distance))
+
+    # check if sound level exceeds hearing threshold or distance threshold
+    if sound_level > Human_Hearing_Threshold:
+        print(f"The sound level reaching you in the {material} room is {sound_level}. You are not hearing anything.")
+    else:
+        print(f"The sound level reaching you in the {material} room is {sound_level}. you are hearing the noise. ")
